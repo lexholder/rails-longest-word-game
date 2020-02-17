3154678039330268ed3d263e7a5392e9ec5bca8d
Alex Holder <lexholder@gmail.com>
2020-02-17 18:08:59 -0500

    update

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,26 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = (0..9).map { ('a'..'z').to_a[rand(26)] }
+  end
+
+  def score
+    word = params[:question]
+    letters = params[:letters].split('')
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    language = open(url).read
+    lang = JSON.parse(language)
+    @error = "#{word} isn't a word" if lang['found'] == false
+    @winner = 'good job' if lang['found'] == true && check(letters, word) == true
+    @not = 'cheater' if check(letters, word) == false
+  end
+
+  def check(grid, word)
+    word = word.split('')
+    word.all? do |letter|
+      word.count(letter) <= grid.count(letter)
+    end
+  end
+end
